c075c6e09958034be69ba9e1990f1e3d206f9a41
Donghee Han <hdh7485@gmail.com>
2020-08-03 21:44:29 +0900

    Added pid package and launch file

--- script/hovering_pid.py
+++ script/hovering_pid.py
@@ -3,27 +3,35 @@
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Float64
 
 class PositionController:
     def __init__(self):
         rospy.Subscriber('/mocap_node/Drone/pose', PoseStamped, self.pose_callback)
+        rospy.Subscriber('/control_effort', Float64, self.effort_callback)
+
+        self.pid_error_pub = rospy.Publisher('/state', Float64, queue_size=1)
+        self.set_point_pub = rospy.Publisher('/setpoint', Float64, queue_size=1)
+
         self.joy_pub = rospy.Publisher('/joy', Joy, queue_size=1)
 
         self.reference_position = [0, 0, 1]
         self.P_GAIN = 0.6
-        self.z_bias = 0.6
+        self.z_bias = 0.5
 
         self.joy_throttle_output = -1
 
     def pose_callback(self, pose_data):
+        self.z_error = self.reference_position[2] - pose_data.pose.position.z
+        self.pid_error_pub.publish(self.z_error)
+        self.set_point_pub.publish(Float64(data=0.0))
 
-        z_error = self.reference_position[2] - pose_data.pose.position.z
-        self.joy_throttle_output = (self.z_bias + z_error * self.P_GAIN) * 2 - 1
+    def effort_callback(self, effort_data):
+        self.joy_throttle_output = (self.z_bias*2-1) + effort_data.data
         pub_joy_msg = Joy(axes=[0,0,0,0,0,0,0])
-
         pub_joy_msg.axes[1] = self.joy_throttle_output
-        self.joy_pub.publish(pub_joy_msg)
 
+        self.joy_pub.publish(pub_joy_msg)
         rospy.loginfo(pub_joy_msg)
 
 if __name__ == '__main__':
